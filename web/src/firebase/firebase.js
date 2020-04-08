50d3bec7d6c22884cb49c109610584b5cfd9aa56
iyke-beezy <isaacbotwe7@gmail.com>
2020-04-08 07:53:07 +0000

    OAuth updates

--- web/src/firebase/firebase.js
+++ web/src/firebase/firebase.js
@@ -3,9 +3,14 @@ import 'firebase/auth';
 //import 'firebase/database';
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
-    //databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
+ apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: "hostelwiz",
+    storageBucket: "hostelwiz.appspot.com",
+    messagingSenderId: "1096528947592",
+    appId: "1:1096528947592:web:d18a691e334a52c2c0b23d",
+    measurementId: "G-CED24MJ2C3"
   });
   
   export default app;
