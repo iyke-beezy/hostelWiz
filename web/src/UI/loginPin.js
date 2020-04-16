97521f4098345e9cbe711de5f1bfb3c2d7cef6dc
MuniruIssah <munirissah2092@gmail.com>
2020-04-16 12:14:37 -0700

    made changes to LoginUi and added the pin component

---
+++ web/src/UI/loginPin.js
@@ -0,0 +1,99 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import PinInput from "react-pin-input";
+import {Button} from 'antd';
+import './loginUI.css'
+import UserPass from "./userPass";
+
+class Pin extends React.Component {
+
+  props={
+    code:PropTypes.number,
+    fname:PropTypes.string,
+    email:PropTypes.string,
+    number:PropTypes.string
+  }
+  state = {
+    value: "",
+    disabled:false,
+    verify:false,
+    continue:false
+     };
+
+  onChange = value => {
+    if(value.length===4){
+      this.setState({ 
+        disabled:true,
+      });
+      if(value===String(this.props.code)){
+        this.setState({ 
+          verify:true
+        });        
+      }
+    }
+    this.setState({ value });
+  };
+
+  onContinue=()=>{
+    this.setState({ 
+        continue:true,
+      });
+
+  }
+
+ /* onClear = () => {
+    this.setState({
+      value: "",
+      disabled:false,
+      verify:false
+      
+    });
+    this.pin.clear();
+  };*/
+
+  render() {
+    const { disabled,verify } = this.state;
+    const{code,fname,email,number}=this.props;
+    let view;
+    if(this.state.continue){
+        view=<UserPass fname={fname} email={email} number={number}/>
+
+    }else{
+        view=
+        <div className="mt-8">
+        <PinInput
+          length={4}
+          focus
+          style={{
+            padding: '20px',
+
+        }}  
+          inputStyle={{borderColor: '#808080', borderWidth:'1.0px', width:'70px',height:'70px', margin: '10px',boxShadow: "1px 1px 1px #9E9E9E"}}
+          inputFocusStyle={{borderColor: '#E7C654'}}
+          disabled={disabled}     
+          ref={p => (this.pin = p)}
+          type="numeric"
+          onChange={this.onChange}
+          
+        />
+        <div className="mt-8">
+            <h2>{code}</h2>
+        </div>
+        <div className="mt-8">
+            <Button size="large" disabled={!verify} onClick={this.onContinue}>
+                Continue
+            </Button>
+        </div>
+      </div>
+    }
+
+
+
+    return (
+        {view}
+    );
+  }
+}
+export default Pin;
+
+
--- web/src/UI/loginUI.js
+++ web/src/UI/loginUI.js
@@ -3,12 +3,100 @@ import PropTypes from 'prop-types';
 import './loginUI.css'
 import { Input, Button } from 'antd';
 import { UserOutlined, MailOutlined, PhoneOutlined } from '@ant-design/icons';
+import Pin from './loginPin';
 
 const propTypes = {
     children: PropTypes.node.isRequired,
 };
-const LoginUI = ({children }) => {
-   
+const code= Math.floor(1000+Math.random()*9000);
+class LoginUI extends React.Component{
+
+    state={
+        move:false,
+        name:'',
+        email:'',
+        number:''
+    }
+
+    handleChange=({ target })=> {
+        this.setState({
+          [target.name]: target.value
+        });
+      }
+      nextPage=()=>{
+        this.setState({
+           move: true
+          });
+    }
+
+
+
+   render(){
+    let display;
+    if (move){
+        display=
+        <div className="mainForm">
+        <h1>Discover More</h1>
+        {/*  */}
+        <div className="mt-8">
+            <Pin code={code} fname={this.state.name} email={this.state.email} number={this.state.number}/>
+        </div>
+        <div className="login">
+            <h3>Already have an account? <Button type="link">Login</Button></h3>
+        </div>
+        <p>
+            Or login with other accounts
+        </p>
+            {children}
+        <div className="footer">
+            <Button type="link">Privacy Policy</Button>
+            <Button type="link">Terms and Conditions</Button>
+            <Button type="link" className="copyright">{'\u00A9'}2020 Slitcorp. All Rights Reserved</Button>
+        </div>
+    </div> 
+        
+    }else{
+        display=
+        <div className="mainForm">
+        <h1>Discover More</h1>
+        {/*  */}
+        <div className="mt-8">
+            <h2>Full Name</h2>
+            <Input placeholder="Enter Full Name" size="large" name="name" prefix={<UserOutlined onChange={this.handleChange}/>}
+            />
+        </div>
+
+        <div className="mt-8">
+            <h2>Email</h2>
+            <Input placeholder="Enter Email" size="large" name="email" prefix={<MailOutlined />} onChange={this.handleChange}/>
+        </div>
+
+        <div className="mt-8">
+            <h2>Phone Number</h2>
+            <Input placeholder="Enter Phone" size="large" name="number" prefix={<PhoneOutlined />} onChange={this.handleChange}/>
+        </div>
+        <div className="mt-8">
+            <Button size="large" onClick={this.nextPage}>
+                Continue
+            </Button>
+        </div>
+        <div className="login">
+            <h3>Already have an account? <Button type="link">Login</Button></h3>
+        </div>
+        <p>
+            Or login with other accounts
+        </p>
+            {children}
+        <div className="footer">
+            <Button type="link">Privacy Policy</Button>
+            <Button type="link">Terms and Conditions</Button>
+            <Button type="link" className="copyright">{'\u00A9'}2020 Slitcorp. All Rights Reserved</Button>
+        </div>
+    </div>
+
+
+    }
+
     return (
         <section>
             <div className="imageBox">
@@ -20,45 +108,13 @@ const LoginUI = ({children }) => {
                         alt="logo"
                     />
                 </div>
-                <div className="mainForm">
-                    <h1>Discover More</h1>
-                    {/*  */}
-                    <div className="mt-8">
-                        <h2>Full Name</h2>
-                        <Input placeholder="Enter Full Name" size="large" prefix={<UserOutlined />}
-                        />
-                    </div>
+                {display}
 
-                    <div className="mt-8">
-                        <h2>Email</h2>
-                        <Input placeholder="Enter Email" size="large" prefix={<MailOutlined />} />
-                    </div>
-
-                    <div className="mt-8">
-                        <h2>Phone Number</h2>
-                        <Input placeholder="Enter Phone" size="large" prefix={<PhoneOutlined />} />
-                    </div>
-                    <div className="mt-8">
-                        <Button size="large">
-                            Continue
-                        </Button>
-                    </div>
-                    <div className="login">
-                        <h3>Already have an account? <Button type="link">Login</Button></h3>
-                    </div>
-                    <p>
-                        Or login with other accounts
-                    </p>
-                        {children}
-                    <div className="footer">
-                        <Button type="link">Privacy Policy</Button>
-                        <Button type="link">Terms and Conditions</Button>
-                        <Button type="link" className="copyright">{'\u00A9'}2020 Slitcorp. All Rights Reserved</Button>
-                    </div>
-                </div>
             </div>
         </section>
-    )
+    );
+
+   }
 }
 
 LoginUI.propTypes = propTypes;
