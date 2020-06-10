a608d4a21ff08695b55558c18497fae577069578
MuniruIssah <munirissah2092@gmail.com>
2020-06-09 22:28:05 -0700

    Modal

--- web/src/components/misc/property.js
+++ web/src/components/misc/property.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { Skeleton, Switch, Card, Avatar } from 'antd';
+import { Skeleton, Switch, Card, Avatar,Modal,Button,Divider,Space } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import Slider from '../productSlider'
@@ -19,12 +19,32 @@ const defaultProps = {
 const { Meta } = Card;
 
 class PropertyItem extends React.Component {
+    state={
+        visible:false
+    }
+    showModal = () => {
+        this.setState({
+          visible: true,
+        });
+      };
+    
+      handleOk = () => {
+        this.setState({
+            visible: false,
+          });
+      };
+    
+      handleCancel = () => {
+        this.setState({ visible: false });
+      };
     render() {
         return (
+            <div>
             <Card
                 hoverable cover={<Slider images={this.props.property.images} />}
                 loading={this.props.loading}
                 style={{borderRadius: "16px 16px 0px 0px", width:'300px'}}
+                onClick={this.showModal}
             >
                 <div style={{display:'flex',justifyContent:'space-between'}}>
                 <div>
@@ -61,6 +81,40 @@ class PropertyItem extends React.Component {
 
 
             </Card>
+            <Modal
+          visible={this.state.visible}
+          width={800}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={[
+           <Button key="back" onClick={this.handleCancel}>
+            Return
+          </Button>,
+            <Button key="link" type="primary"  onClick={this.handleOk}>
+              View On Map
+            </Button>,
+            <Button key="submit" type="primary" onClick={this.handleOk}>
+              Call Host
+            </Button>,
+          ]}
+        >
+                <h1>{this.props.property.name}</h1>
+                <Divider/>
+                <Slider images={this.props.property.images} />
+                <Space style={{height:'50px'}}></Space>
+                <Divider/>
+
+                <div>
+                <h2>Location</h2>
+                </div>
+                <Divider/>
+                <div>
+                <h2>Amenities</h2>
+                </div>
+            
+
+        </Modal>           
+            </div>
 
 
         )
