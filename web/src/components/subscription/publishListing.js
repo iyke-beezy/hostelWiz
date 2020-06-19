68b45d38c7c548f58feaad1629982c81f3b344ee
MuniruIssah <munirissah2092@gmail.com>
2020-06-19 05:44:03 -0700

    yh

--- web/src/components/subscription/publishListing.js
+++ web/src/components/subscription/publishListing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu,Progress,Button ,Tabs} from 'antd';
+import { Layout, Menu,Progress,Button ,Tabs, Divider} from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import './hostForm.css';
@@ -23,16 +23,86 @@ class PublishListing extends React.Component{
          security=JSON.parse(localStorage.getItem('security'));
          propertyDetails=JSON.parse(localStorage.getItem('propertyDetails'));
          addPhotos=JSON.parse(localStorage.getItem('addPhotos'));
-         console.log(locationDetails);
-         console.log(pricing);
-         console.log(security);
-         console.log(propertyDetails);
-         console.log(addPhotos)
+        this.setState({
+            locationDetails:locationDetails,
+            pricing:pricing,
+            security:security,
+            propertyDetails:propertyDetails,
+            addPhotos:addPhotos
+        })
     };
     render(){
 
         return(
-            <div></div>
+            <div>
+                <div>
+                <span className='medText'>Location Details</span>
+                
+        <div>{
+            this.state.locationDetails?
+            <div>
+            <span>{this.state.locationDetails.region}</span>
+            <span>{this.state.locationDetails.streetAddress}</span>
+            <span>{this.state.locationDetails.city}</span>
+            </div>
+           
+    :null
+    }</div>
+
+                </div>
+                <div>
+                <span className='medText'>Property Details</span>
+                
+        <div>{
+            this.state.propertyDetails?
+            <div>
+            <span>{this.state.propertyDetails.headline}</span>
+            <span>{this.state.propertyDetails.description}</span>
+            <span>{this.state.propertyDetails.propertyType}</span>
+            <span>{this.state.propertyDetails.bedrooms}</span>
+            <span>{this.state.propertyDetails.accomodates}</span>
+            <span>{this.state.propertyDetails.bathrooms}</span>
+           
+            </div>
+           
+    :null
+    }</div>
+                </div>
+                <div>
+                <span className='medText'>Property Photos</span>
+      <div>{
+            this.state.addPhotos.fileList?
+            this.state.addPhotos.fileList.map(fileList=><span key={fileList.uid}>{fileList.name}</span>)
+            :null
+    }</div>
+                </div>
+                <div>
+                <span className='medText'>Security</span>
+                <div>{
+            this.state.security.numbers?
+                this.state.security.numbers.map(number=><span key={number}>{number}</span>)
+            :null
+            }</div>
+                </div>
+                <div>
+                <span className='medText'>Pricing</span>
+                
+        <div>{
+            this.state.pricing?
+            <div>
+            <span>{this.state.pricing.price}</span>
+            <span>{this.state.pricing.time}</span>
+       
+            </div>
+           
+    :null
+    }</div>                
+                </div>
+            <div className='dC1'>
+            <Button className='finalButton' onClick={this.props.handleBack}>Back</Button>
+                <Button className='finalButton'>Next</Button> 
+            </div>
+            </div>
         )
     }
 }
